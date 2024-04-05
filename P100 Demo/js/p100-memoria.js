b865111f3d10645bcea2e196d190209091aed5ba
Noel Sariñena Varela <nsarinenav@edu.tecnocampus.cat>
2024-04-05 18:31:52 +0200

    Distribució de taulell finalitzat

--- P100 Demo/js/p100-memoria.js
+++ P100 Demo/js/p100-memoria.js
@@ -1,19 +1,28 @@
 var ampladaCarta, alcadaCarta;
 var separacioH=20, separacioV=20;
-var nFiles=2, nColumnes=2;
+var nFiles=3, nColumnes=3;
 
 var jocCartes = [
     'carta14',
-    'carta15'
+    'carta15',
+    'carta16'
 ];
 
 $(function(){
-
     // mida del tauler
+        // 2 x 2 => 40
+        // 3 x 3 => 60
+        // 4 x 4 => 80
+    let totalRestar = 0
+    totalRestar = nFiles != 1 ? (20 * nFiles) - 20 : 0
+    console.log(totalRestar)
     $("#tauler").css({
-        "width" : `${110 * nColumnes}px`,
-        "height": `${150 * nFiles}px`
+        "width" : `${120 * nColumnes - totalRestar}px`,
+        "height": `${160 * nFiles - totalRestar}px`
     });
+    
+    ampladaCarta=$(".carta").width(); 
+    alcadaCarta=$(".carta").height();
 
     for (i = 0; i < nFiles; i++) {
         for (j = 0; j < nColumnes; j++) {
@@ -30,20 +39,14 @@ $(function(){
 function generarCarta(f, c) {
 
     let cartaID = `f${f}c${c}`;
-    //style="left:${((f-1)*(alcadaCarta+separacioV)+separacioV)}px; top: ${((c-1)*(ampladaCarta+separacioH) +separacioH)}px"
     let cartaHTML = `<div class="carta" id="${cartaID}"><div class="cara darrera"></div><div class="cara davant"></div></div>`;
     $('#tauler').append(cartaHTML);
-
-    ampladaCarta=$(".carta").width(); 
-    alcadaCarta=$(".carta").height();
 
     let carta = $(`#${cartaID}`);
     carta.css({
         "top" :  ((f-1)*(alcadaCarta+separacioV)+separacioV)+"px",
-        "left"  :  ((c-1)*(ampladaCarta+separacioH) +separacioH)+"px"
+        "left"  :  ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px"
     });
-    //$('#tauler').append(cartaHTML);
     
-    //let carta = $("#f"+f+"c"+c);
     carta.find(".davant").addClass(jocCartes.pop());
 }
