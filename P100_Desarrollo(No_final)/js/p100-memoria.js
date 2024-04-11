e0a43848178c210976346adc3848b43ced209f72
Noel Sariñena Varela <nsarinenav@edu.tecnocampus.cat>
2024-04-11 11:24:32 +0200

    Comentaris

--- P100_Desarrollo(No_final)/js/p100-memoria.js
+++ P100_Desarrollo(No_final)/js/p100-memoria.js
@@ -47,51 +47,9 @@ $(function(){
         }
     }
 
-    // FIXME: Comentar correctamente
+    controlarCartes();
 
-    // Funció que salta quan es fa click sobre alguna de les cartes
-    var contadorClics = 0;
-    var par1, par2;
-    $(".carta").on("click", function() {
-        $(this).toggleClass("carta-girada"); //CON ESTO SE GIRAN LAS CARTAS
-        if(contadorClics === 0){
-            par1=$(this);
-        }
-        
-        contadorClics++;
-        console.log(contadorClics);
-        if (contadorClics === 2) {
-            par2 = $(this);
-            // Obtiene las clases de las caras delanteras de las cartas
-            let clasePar1 = par1.find(".davant").attr('class');
-            let clasePar2 = par2.find(".davant").attr('class');
-        
-            if (clasePar1 == clasePar2) {
-                setTimeout(function() {
-                    par2.hide();
-                    par1.hide();
-                }, 1000); // Retraso de 1 segundo
-            } else {
-                setTimeout(function() {
-                    $(par1).toggleClass("carta-girada");
-                    $(par2).toggleClass("carta-girada");
-                }, 1000); // Retraso de 1 segundo
-            }
-            contadorClics = 0;
-        }        
-    });
-
-    // Temporizador
-    var tiempoRestante = 100; 
-    var temporizador = setInterval(function() {
-        tiempoRestante--;
-        $("#temporitzador").text(tiempoRestante);
-        if (tiempoRestante <= 0) {
-            clearInterval(temporizador);
-            $("#temporitzador").text("El temps s'ha esgotat");
-            // Acciones cuando el tiempo se acabe
-        }
-    }, 1000); 
+    temporitzadorJoc();
 });
 
 /**
@@ -146,4 +104,58 @@ function iniciarJuego(dificultad) {
     // Aquí puedes expandir la lógica para generar el tablero según la dificultad
     
     console.log('Dificultad seleccionada:', dificultad);
+}
+
+/**
+ * Funció que escolta als clicks del ratolí
+ */
+function controlarCartes () {
+    // Funció que salta quan es fa click sobre alguna de les cartes
+    let contadorClics = 0;
+    let par1, par2;
+    $(".carta").on("click", function() {
+        $(this).toggleClass("carta-girada"); //CON ESTO SE GIRAN LAS CARTAS
+        if(contadorClics === 0){
+            par1=$(this);
+        }
+        
+        contadorClics++;
+        console.log(contadorClics);
+        if (contadorClics === 2) {
+            par2 = $(this);
+            // Obtiene las clases de las caras delanteras de las cartas
+            let clasePar1 = par1.find(".davant").attr('class');
+            let clasePar2 = par2.find(".davant").attr('class');
+        
+            if (clasePar1 == clasePar2) {
+                setTimeout(function() {
+                    par2.hide();
+                    par1.hide();
+                }, 1000); // Retraso de 1 segundo
+            } else {
+                setTimeout(function() {
+                    $(par1).toggleClass("carta-girada");
+                    $(par2).toggleClass("carta-girada");
+                }, 1000); // Retraso de 1 segundo
+            }
+            contadorClics = 0;
+        }        
+    });
+}
+
+/**
+ * Funció que controla el temporitzador del joc
+ */
+function temporitzadorJoc () {
+    // Temporizador
+    let tiempoRestante = 100; 
+    let temporizador = setInterval(function() {
+        tiempoRestante--;
+        $("#temporitzador").text(tiempoRestante);
+        if (tiempoRestante <= 0) {
+            clearInterval(temporizador);
+            $("#temporitzador").text("El temps s'ha esgotat");
+            // Acciones cuando el tiempo se acabe
+        }
+    }, 1000); 
 }
