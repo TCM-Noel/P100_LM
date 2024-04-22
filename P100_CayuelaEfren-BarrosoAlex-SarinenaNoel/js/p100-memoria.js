f2f7fc216b6ff4f2eac9b6e3bd9841073400bbf7
Noel Sariñena Varela <noelsava25@gmail.com>
2024-04-22 10:29:35 +0200

    Push produccion

--- P100_CayuelaEfren-BarrosoAlex-SarinenaNoel/js/p100-memoria.js
+++ P100_CayuelaEfren-BarrosoAlex-SarinenaNoel/js/p100-memoria.js
@@ -1,32 +1,65 @@
 // Variables globals
-var ampladaCarta, alcadaCarta;
-var separacioH=20, separacioV=20;
-var nFiles=2, nColumnes=2;
-var numCartesJugar = 0;
+var separacioH=20, separacioV=20; // Separació entre cartes
+var nFiles=2, nColumnes=2; // Files i columnes
+var numCartesJugar = 0; // Numero que escull l'usuari per jugar cartes
+var numCartesMa = 52; // Mà "deck" per defecte
+var guanyat=false;
 
-// Cartes de cada mà
-var maCartesEspanyoles = 52;
-
-// Dues mans amb la que es generarà el joc
+// Mà amb la que es genera el joc
+var ampladaCarta = 80, alcadaCarta = 120; // Se li assigna l'alçada i l'amplada del taulell dinàmicament
 var barallaMa = [];
+var maTriada = 'carta';
+var maDavant = 'davant';
+var maDarrera = 'darrera';
+var cartaWidth = 120;
+var cartaHeigh = 160;
 
 /**
- * Funció que comproba el resultat de l'input del menú 
+ * Funció que executa el joc segons les dues modalitats en les que es pot iniciar:
+ *      - Si es prem un botó de mode, entra directament al joc
+ *      - Si tria el numero de cartes, es verifica que siguin correctes i inicia el joc
  */
-function comprobarCartes () {
-    numCartesJugar = parseInt($('#numCartes').val());
-    if (numCartesJugar!="" && numCartesJugar%2===0 && numCartesJugar<=maCartesEspanyoles && numCartesJugar > 0) {
-        $('#missatgeError').css('display', 'none')
+function comprobarCartes (numCartes) {
+    if (numCartes !== 0) {
+        numCartesJugar = numCartes;
+        comprobacio = true;
+    } else {
+        comprobacio = comprobarNumCorrecte();
+    }
+    if (comprobacio) {
         $('#menuInicial').css('display', 'none')
         $('#footer').css('display', 'block')
         $('#tauler').css('display', 'block')
+        $('#temporitzador').show();
+        reproducirSonidotaulell();
         iniciaJoc();
     } else {
-        $('#missatgeError').css('display', 'block')
         $('#footer').css('display', 'none')
         $('#tauler').css('display', 'none')
     }
 }
+
+/**
+ * Funció que posa l'input en diferents colors èr
+ * @returns Comprobació si el numero introduit pel jugador
+ */
+function comprobarNumCorrecte () {
+    inp = $('#numCartes');
+    numCartesJugar = parseInt(inp.val());
+    if (numCartesJugar!="" && numCartesJugar%2===0 && numCartesJugar<= numCartesMa && numCartesJugar > 0) {
+        inp.removeClass('numIncorrecte');
+        return true;
+    } else {
+        if (isNaN(numCartesJugar) || numCartesJugar === 0) {
+            inp.val('');
+            inp.attr('placeholder', 'Introdueix un numero...')
+            inp.removeClass('numIncorrecte');
+        } else {
+            inp.addClass('numIncorrecte');
+        }
+        return false;
+    }
+}   
 
 /**
  * Funció que genera tot el joc
@@ -40,7 +73,7 @@ function iniciaJoc () {
     let comptadorCartes = 0;
     for (i = 0; i < nFiles; i++) {
         for (j = 0; j < nColumnes; j++) {
-            if (comptadorCartes != numCartesJugar) {
+            if (comptadorCartes !== numCartesJugar) {
                 generarCarta(i+1, j+1);
                 comptadorCartes++;
             }
@@ -53,12 +86,50 @@ function iniciaJoc () {
 }
 
 /**
+ * Funció que escolta als botons que trien cada mà i aplica les configuracions òptimes
+ * @param jugaAmb Cartes amb les que es juga
+ * @param carta Passa la carta seleccionada
+ */
+function numCartesDeMa (jugaAmb, carta, maTriadaHTML, maDavantHTML, maDarreraHTML) {
+    numCartesMa = jugaAmb;
+    $('.maTriadaJoc').css({
+        'filter' : 'grayscale(90%)',
+        'box-shadow' : '0px 0px 0px 0px'
+    })
+    $(carta).css({
+        'filter' : 'none',
+        'box-shadow' : 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px'
+    })
+    maTriada = maTriadaHTML;
+    maDavant = maDavantHTML;
+    maDarrera = maDarreraHTML;
+    if (maTriada == 'carta') {
+        cartaWidth = 120;
+        cartaHeigh = 160;
+        ampladaCarta = 80;
+        alcadaCarta = 120;
+    } else if (maTriada == 'cartaP') {
+        cartaWidth = 151;
+        cartaHeigh = 151;
+        ampladaCarta = 111;
+        alcadaCarta = 111;
+    } else {
+        cartaWidth = 0; // FIXME: Emplenar
+        cartaHeigh = 0; // FIXME: Emplenar
+        ampladaCarta = 0; // FIXME: Emplenar
+        alcadaCarta = 0; // FIXME: Emplenar
+    }
+    comprobarNumCorrecte();
+}
+/**
  * Funció que genera la mà i controla el numero de cartes
  */
 function crearMa () {
     // S'afegeixen totes les cartes a l'array 'barallaMa'.
-    for(let i = 1; i <= 52; i++){
-        barallaMa.push('carta' + i);
+    for(let i = 1; i <= numCartesMa; i++){
+        // Condició que controla que la mà no sigui "cartaP" i la carta no sigui la 8 (la part de darrera de les cartes)
+        if (maTriada === 'cartaP' && i === 8) barallaMa.push(maTriada + i + 1); 
+        else barallaMa.push(maTriada + i);
     }
 
     // Gestió de columnes i files
@@ -77,25 +148,31 @@ function crearMa () {
 }
 
 /**
+ * FIXME: Solucionar cálculos
  * Fa un càcul per tal de quadrar correctament les columnes i així tenir una bona distribució del taulell
  * @returns nColumnes
  */
 function trobarFactor () {
 
     let divisors = [];
+
+    // Iteració que controla que els divisors no siguin ni 1 ni el propi numero
     for (i = 1; i <= numCartesJugar; i++) {
         if (numCartesJugar%i === 0 && i !== 1 && i !== numCartesJugar) divisors.push(i);
     }
-    if (divisors.length === 1) {
+
+    if (divisors.length === 1) { // Retorna el divisor en cas de que només existeixi un
         return divisors[0];
     } else {
-        let divisorGran = divisors[Math.floor(divisors.length / 2)];
-        let divisorPetit = divisors[Math.floor(divisors.length / 2)-1];
-        if (divisorPetit+1 != divisorGran) {
-            let diferencia = (divisorGran - divisorPetit);
-            return divisorGran-(Math.floor(diferencia/2));
+        let divisorMigGran = divisors[Math.round(divisors.length / 2)];
+        let divisorMigPetit = divisors[Math.round(divisors.length / 2)-1];
+        if (divisorMigPetit+1 != divisorMigGran) {
+            let diferencia = (divisorMigGran - divisorMigPetit);
+            if((divisorMigGran-(Math.round(diferencia/2)))%2==1){return divisorMigGran-(Math.round(diferencia/2))+1; }
+            return divisorMigGran-(Math.round(diferencia/2));
         }
-        return divisorGran;
+        if(divisorMigGran%2==1) divisorMigGran+1;
+        return divisorMigGran;
     }
 }
 
@@ -107,16 +184,22 @@ function midesGenerals () {
         // 2 x 2 => 20
         // 3 x 3 => 40
         // 4 x 4 => 60
-    let totalRestarFiles = nFiles != 1 ? 20 * (nFiles - 1) : 0;
-    let totalRestarColumnes = nFiles != 1 ? 20 * (nColumnes - 1) : 0;
+    let totalRestarFiles = 0;
+    let totalRestarColumnes = 0;
+
+    if (nFiles > 1) {
+        totalRestarFiles = 20 * (nFiles - 1);
+    }
+
+    if (nColumnes > 1) {
+        totalRestarColumnes = 20 * (nColumnes - 1);
+    }
+
     $("#tauler").css({
-        "width" : `${120 * nColumnes - totalRestarColumnes}px`,
-        "height": `${160 * nFiles - totalRestarFiles}px`
+        "width" : `${cartaWidth * nColumnes - totalRestarColumnes}px`,
+        "height": `${cartaHeigh * nFiles - totalRestarFiles}px`
     });
 
-    // Agafa les mesures de les cartes
-    ampladaCarta=$(".carta").width(); 
-    alcadaCarta=$(".carta").height();
 }
 
 /**
@@ -142,15 +225,15 @@ function barrejar(array) {
  */
 function generarCarta(f, c) {
     let cartaID = `f${f}c${c}`;
-    let cartaHTML = `<div class="carta" id="${cartaID}"><div class="cara darrera"></div><div class="cara davant"></div></div>`;
+    let cartaHTML = `<div class="${maTriada}" id="${cartaID}"><div class="cara ${maDarrera}"></div><div class="cara ${maDavant}"></div></div>`;
     $('#tauler').append(cartaHTML);
 
     let carta = $(`#${cartaID}`);
     carta.css({
-        "top" :  ((f-1)*(alcadaCarta+separacioV)+separacioV)+"px",
-        "left"  :  ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px"
+        "top"  :  ((f-1)*(alcadaCarta+separacioV)+separacioV)+"px",
+        "left" :  ((c-1)*(ampladaCarta+separacioH)+separacioH)+"px"
     });
-    carta.find(".davant").addClass(barallaMa.pop());
+    carta.find(`.${maDavant}`).addClass(barallaMa.pop());
 }
 
 /**
@@ -160,34 +243,37 @@ function controlarCartes () {
     // Funció que salta quan es fa click sobre alguna de les cartes
     let contadorClics = 0;
     let par1, par2;
-    $(".carta").on("click", function() {
+    $(`.${maTriada}`).on("click", function() {
         if (contadorClics === 1 && par1[0] === this) {
-            return; // No hacer nada si se hace clic en la misma carta
+            return; // No fer res si es fa clic a la mateixa carta.
         }
-        $(this).toggleClass("carta-girada"); //CON ESTO SE GIRAN LAS CARTAS
+        $(this).toggleClass("carta-girada"); // Auqesta funció gira les cartes
         if(contadorClics === 0){
             par1=$(this);
         }
         
-        contadorClics++;
-        console.log(contadorClics);
+        contadorClics++; 
         if (contadorClics === 2) {
             par2 = $(this);
-            // Obtiene las clases de las caras delanteras de las cartas
-            let clasePar1 = par1.find(".davant").attr('class');
-            let clasePar2 = par2.find(".davant").attr('class');
+            // Obté les classes de les cares davanteres de les cartes
+            let clasePar1 = par1.find(`.${maDavant}`).attr('class');
+            let clasePar2 = par2.find(`.${maDavant}`).attr('class');
+            $(`.${maTriada}`).addClass('noClick'); // Afegeix la classe noClick 
         
-            if (clasePar1 == clasePar2) {
-                setTimeout(function() {
+            // Timeout que comprova les cartes clickades
+            setTimeout(function() {
+                if (clasePar1 == clasePar2){
                     par2.hide();
                     par1.hide();
-                }, 1000); // Retraso de 1 segundo
-            } else {
-                setTimeout(function() {
+                    verificarFinJuego();
+                } else {
                     $(par1).toggleClass("carta-girada");
                     $(par2).toggleClass("carta-girada");
-                }, 1000); // Retraso de 1 segundo
-            }
+                }
+                console.log(maTriada)
+                $(`.${maTriada}`).removeClass('noClick'); // Elimina la classe noClick que bloqueja els events
+            }, 1000); // Retard d'un segon
+            
             contadorClics = 0;
         }        
     });
@@ -198,14 +284,91 @@ function controlarCartes () {
  */
 function temporitzadorJoc () {
     // Temporizador
-    let tiempoRestante = 100; 
+    
+    let tiempoRestante = 3*numCartesJugar;
+    $("#temporitzador").animate({ value: tiempoRestante }, 1000);
+    $("#temporitzador").attr("max", tiempoRestante);
     let temporizador = setInterval(function() {
-        tiempoRestante--;
-        $("#temporitzador").text(tiempoRestante);
-        if (tiempoRestante <= 0) {
-            clearInterval(temporizador);
-            $("#temporitzador").text("El temps s'ha esgotat");
-            // Acciones cuando el tiempo se acabe
+        if(!guanyat){
+            tiempoRestante--;
+            $("#temporitzador").animate({ value: tiempoRestante }, 1000);
+            if(tiempoRestante<=5){ // Quan quedin 5 segons, es posa una música de tensió per advertir a l'usuari
+                reproducirSonidoPocoTiempo();
+            }
+            if (tiempoRestante <= 0) { // Accions que es realitzen quan el temps s'acaba
+                clearInterval(temporizador);
+                setTimeout(function() {
+                    //$(`.${maTriada}`).hide();
+                    pausarSonidoPocoTiempo();
+                    pausarSonidotaulell();
+                    senseTemps();
+                    verificarFinJuego(true);
+                }, 1000);
+            }    
         }
+        
     }, 1000); 
+}
+
+/**
+ * Funció que torna al menú principal
+ */
+function tornarAlMenu(){
+    location.href="index.html";
+}
+
+/**
+ * Funcions de so
+ */
+function senseTemps(){
+    let audio = document.getElementById("perdut");
+    audio.play();
+}
+function pausarSonidoPocoTiempo(){
+    let audio = document.getElementById("PocTemps");
+    audio.pause();
+}
+function pausarSonidoMenu() {
+    let audio = document.getElementById("menuSound");
+    audio.pause();
+}
+function pausarSonidotaulell() {
+    let audio = document.getElementById("taulerSound");
+    audio.pause();
+}
+function reproducirSonidoPocoTiempo(){
+    let audio = document.getElementById("PocTemps");
+    audio.play();
+}
+function reproducirSonidoMenu() {
+    let audio = document.getElementById("menuSound");
+    audio.play();
+}
+function reproducirSonidotaulell() {
+    let audio = document.getElementById("taulerSound");
+    audio.play();
+}
+
+/**
+ * Funció que finalitza el joc
+ */
+function verificarFinJuego(tiempoAgotado) {
+    let cartas = $(`.${maTriada}`); 
+    let todasOcultas = true;
+
+    cartas.each(function() {
+        if ($(this).css('display') !== 'none') {
+            todasOcultas = false;
+        }
+    });
+
+    if (todasOcultas) {
+        guanyat=true;
+        alert('¡Felicidades! Has completado el juego.');
+        tornarAlMenu();
+    } else if (tiempoAgotado) {
+        guanyat = false;
+        alert('¡Tiempo agotado! Has perdido.');
+        tornarAlMenu();
+    }
 }
